509293723a83d28abb2c8775c001fd6b444d8ae4
philip <philster36@hotmail.co.uk>
2023-02-15 13:09:46 +0000

    painful receipt styling

--- src/components/CoffeeShop/CoffeeShop.jsx
+++ src/components/CoffeeShop/CoffeeShop.jsx
@@ -2,14 +2,12 @@ import React, { useState, useEffect, useRef } from "react";
 import "./CoffeeShop.css"
 
 const createItem = (item, itemQuanities, setQuantity) => {
-
     const updateItem = (value) => {
         const amount = itemQuanities[item.id] + value;
         if((itemQuanities[item.id] + amount) <= 0) return;
         setQuantity({ ...itemQuanities, [item.id]: amount})
         console.log(`${item.id}`)
     }
-
 
     return <div className="item-holder">
         <div className="item-desc">
@@ -24,8 +22,22 @@ const createItem = (item, itemQuanities, setQuantity) => {
     </div>;
 };
 
+const createReceiptItem = (name, quantity, price) => {
+    const priceAfterQuantity = quantity * price;
+
+    return (
+        <div className="receipt-item-wrapper">
+            <p className="fixed-rcpt">{name}</p>
+            <p className="fixed-rcpt">{quantity}</p>
+            <p className="fixed-rcpt">£{price.toFixed(2)}</p>
+            <p className="fixed-rcpt">£{priceAfterQuantity.toFixed(2)}</p>
+        </div> 
+    )
+}
+
 export default function CoffeeShop({ items }) {
     const itemsToDisplay = [];
+    const [receiptItems, setReceiptItems] = useState([]);
     const [itemQuantity, setItemQuantity] = useState({});
     const [totalPrice, setTotalPrice] = useState(0);
 
@@ -47,9 +59,15 @@ export default function CoffeeShop({ items }) {
     
     useEffect(() => {
         let _totalPrice = 0
+        let newArr = []
         for (const id in itemQuantity) {
             const item = items.find(item => item.id == id)
             _totalPrice += item.price * itemQuantity[id]
+
+            if(itemQuantity[id] > 0) {
+                newArr.push(createReceiptItem(item.name, itemQuantity[id], item.price));
+            }
+            setReceiptItems(newArr)
         }
         setTotalPrice(_totalPrice);
     }, [itemQuantity])
@@ -57,7 +75,6 @@ export default function CoffeeShop({ items }) {
     items.forEach(item => {
         itemsToDisplay.push(createItem(item, itemQuantity, clickHandle));
     });
-    
     
     return (
         <div>
@@ -70,7 +87,12 @@ export default function CoffeeShop({ items }) {
                     {itemsToDisplay}
                 </div>
             </div>
-            <div>£{totalPrice.toFixed(2)}</div>
+            <div id="bottom-wrapper">
+                <div id="receipt-wrapper">
+                    {receiptItems}
+                    <div id="receipt-total-price">£{totalPrice.toFixed(2)}</div>
+                </div>
+            </div>
         </div>
     );
 
